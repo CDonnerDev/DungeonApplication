900cf8ef5a8511d80d42f788cb27ac1ce1145585
Colson Donner <Colson.Donner1@outlook.com>
2022-10-21 12:47:44 -0500

    Built 4 Different implementations of monster

---
+++ DungeonLibrary/Rabbit.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DungeonLibrary
+{
+    public class Rabbit : Monster
+    {
+        //FIELDS
+        //no fields
+
+        //Properties
+        public bool IsFluffy { get; set; }
+        //Constructors  
+        //Default Ctor to set some basic values for a generic monster of this type
+        public Rabbit()
+        {
+            MaxLife = 6;
+            MaxDamage = 3;
+            Name = "Baby Rabbit";
+            Life = MaxLife;
+            HitChance = 20;
+            Block = 20;
+            MinDamage = 1;
+            Description = "It's just a cute little bunny...why would you hurt it";
+            IsFluffy = false;
+        }
+        //Parent Complaint (Monster) CTOR
+        //Intellisense quick action on the parent name in the class declaration
+        public Rabbit(string name, int hitChance, int block, int maxLife, //Character
+            int maxDamage, int damage, int minDamage, string description,//Monster
+            bool isFluffy) //Rabbit
+            : base(name, hitChance, block, maxLife, maxDamage, damage, minDamage, description)
+        {
+            IsFluffy=isFluffy;
+        }
+
+        //Methods
+
+        public override string ToString()
+        {
+            return base.ToString() + (IsFluffy ? "It's fluffy" : "It's not so fluffy");
+        }
+
+        //Character CalcBlock = Block
+        //Monster CalcBlock = Block
+        //Rabbit CalcBlock = not block
+        public override int CalcBlock()
+        {
+            int calculatedBlock = Block;
+            //Apply a 50% increase to the rabbits if its fluffy
+            if (IsFluffy)
+            {
+                calculatedBlock += calculatedBlock / 2;
+            }
+            return calculatedBlock;
+        }
+    }
+}
